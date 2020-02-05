3cbf43da81f16b7f8b17cc1f228bb5b580a0d5e3
Ubuntu <ubuntu@ip-172-31-55-66.us-west-2.compute.internal>
2020-02-05 01:15:06 +0000

    fixed minor bugs

--- scripts/create_frames_pl.sh
+++ scripts/create_frames_pl.sh
@@ -18,7 +18,7 @@ echo $RESULTSDIR
 for epoch in {10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,310,320,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,480,490,500}
 do
 echo 'Epoch '$epoch
-CMD='--dataroot '$DATA_ROOT' --name '$DB' --model pix2pix --dataset_mode aligned --gpu_id '$GPU' --load_iter '$epoch' --force_test_output '$RESULTSDIR'/'$DB'_'$epoch' --checkpoints_dir '$CPDIR
+CMD='--dataroot '$DATA_ROOT' --name '$DB' --model pix2pixpl --dataset_mode aligned --gpu_id '$GPU' --load_iter '$epoch' --force_test_output '$RESULTSDIR'/'$DB'_'$epoch' --checkpoints_dir '$CPDIR
 echo $CMD
 python test_nohtml.py $CMD
 done
